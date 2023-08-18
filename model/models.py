fac79f09bef839588ad835c1eaf1b8fb54dec8be
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-18 23:06:48 +0300

    class Role added

--- model/models.py
+++ model/models.py
@@ -12,3 +12,19 @@ class Hero:
         self.image_path = image_path
         self.image = image
         self.roles_set = roles_set
+
+    def __repr__(self):
+        return f"""Hero: {self.name}
+                        dotabuff_name: {self.dotabuff_name}
+                        general_winrate: {self.general_winrate}"""
+
+
+class Role:
+    """ Contains info about specific roles for heroes. """
+
+    def __init__(self, *, id_=None, name=None):
+        self.id = id_
+        self.name = name
+
+    def __repr__(self):
+        return f"Role: '{self.name}'"
