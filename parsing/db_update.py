7d11e51d614346b9ab56398a99f9dffa3f9d6710
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-18 16:44:04 +0300

    Added db_update - chain between parsing and DB

---
+++ parsing/db_update.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def insert_hero_table_from_scratch():
+    """ Get all heroes from dotabuff and insert them into hero table. """
+    pass
+
+
+def update_hero_table_general_winrate():
+    """ Go through all heroes in database and update general winrate. """
+
+
+def update_hero_table_strength_set():
+    """ Go through all heroes in database and update general winrate. """
+
+
+def update_hero_table_position_set():
+    """ Go through all heroes in database and update general winrate. """
