6aa3b4e3b10aa4164fc86594769c10cbd98a829a
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-18 16:42:18 +0300

    Added utils

--- utils/general.py
+++ utils/general.py
@@ -4,4 +4,8 @@ from pathlib import Path
 def get_root_dir():
     """ Return root dir of the DotaPick project. """
 
-    return Path(__file__).parent
+    return Path(__file__).parent.parent
+
+
+def get_resources_dir():
+    return get_root_dir() / Path('resources')
