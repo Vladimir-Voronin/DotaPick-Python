373a5f9cbf8c642f8902133b5f842198fbcc74a3
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-19 13:16:52 +0300

    new tests

--- test_/db/db_completeness_test.py
+++ test_/db/db_completeness_test.py
@@ -24,6 +24,17 @@ class DBCompletenessTest(unittest.TestCase):
 
         self.assertTrue(diff.seconds // 3600 < 1, "Data may not have been updated correctly.")
 
+    def test_heroes_winrate_table(self):
+        # Check if data were updated within 1 hour
+        take_update_date_sql = """SELECT update_date FROM heroes_winrate"""
+
+        self.curs.execute(take_update_date_sql)
+        result = self.curs.fetchall()
+
+        result = [datetime.datetime.fromisoformat(date[0]) for date in result]
+        diff = max(result) - min(result)
+
+        self.assertTrue(diff.seconds // 3600 < 1, "Data may not have been updated correctly.")
 
     def tearDown(self) -> None:
         self.conn.close()
