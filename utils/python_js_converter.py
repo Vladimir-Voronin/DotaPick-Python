2ddf868a12c67ca915d2b13c22c70f2b644d0278
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-20 16:02:11 +0300

    JSON converter (Python -> JS)

---
+++ utils/python_js_converter.py
@@ -0,0 +1,33 @@
+import json
+
+from db.read_api import get_complete_hero_list
+
+
+def convert_hero_list_to_json(hero_list):
+    """ Return json format string for hero_list with transformed hero.winrate_dict."""
+
+    result = []
+
+    for hero in hero_list:
+        transform_winrate_dict_to_concise_form(hero)
+
+        hero.roles_set = tuple(role for role in hero.roles_set)
+        hero_json = json.dumps(hero.__dict__)
+        result.append(hero_json)
+
+    return json.dumps(result)
+
+
+def transform_winrate_dict_to_concise_form(hero):
+    """ Get hero.winrate_dict and transform it to {dotabuff_name: winrate} form.
+
+        It helps to convert hero to json without extra information
+    """
+
+    hero.winrate_dict = {k.dotabuff_name: v for k, v in hero.winrate_dict.items()}
+    return hero
+
+
+if __name__ == '__main__':
+    hero_list = get_complete_hero_list()
+    res = convert_hero_list_to_json(hero_list)
