bb78759cac83e1bc1ebb75430fa535fcfe9efe9a
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-26 17:38:20 +0300

    init update (new functions call)

--- static_web_server/js/innerFunctions.js
+++ static_web_server/js/innerFunctions.js
@@ -15,11 +15,26 @@ const mainObjects = {
 function initMainPageObjects() {
     console.log("loading page...");
     blockUI("init main objects.")
+
+    mainPageDefault();
+
     const promiseCurrentList = getCurrentHeroListFromDB();
     promiseCurrentList
         .then(heroList => {
             mainObjects.heroList = heroList;
             mainObjects.recommendationList = new RecommendationList(heroList);
+            
+            changeCurrentTeamObject(mainObjects.teamEnemy);
+            changeCurrentTeamUIObject(getEnemyTeamUI());
+            
+            updateRecommendationTable();
+            
+            fillMainPage();
+            
+            keyBindingsInit();
+            UIBindings();
+
+
             console.log("Page has loaded, mainObjects have been initialized");
             console.log(mainObjects.recommendationList);
             unblockUI();
