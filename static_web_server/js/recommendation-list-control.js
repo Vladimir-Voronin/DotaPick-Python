3fc8105976ca2494938a6570b37bafafec5c7206
Воронин Владимир <vladimirvoron7@gmail.com>
2023-08-28 16:00:34 +0300

    <- Custom render in recommendation table

--- static_web_server/js/recommendation-list-control.js
+++ static_web_server/js/recommendation-list-control.js
@@ -1,4 +1,40 @@
 /**
+ *  Render function for numbers in recommendation table (>50% - green, <50% - red).
+ * @param {Number} data Inner number to render
+ * @returns 
+ */
+function customRenderNumber50Average(data) {
+    const postfix = "%";
+    const precision = 2;
+
+    if (data >= 50) {
+        return `<div class="positive-number">${data.toFixed(precision)}${postfix}</div>`
+    }
+    else {
+        return `<div class="negative-number">${data.toFixed(precision)}${postfix}</div>`
+    }
+}
+
+/**
+ *  Render function for numbers in recommendation table (>0% - green, ===0% - default, <0% - red).
+ * @param {Number} data Inner number to render
+ * @returns 
+ */
+function customRenderNumberZeroAverage(data) {
+    const postfix = "%";
+    const precision = 2;
+
+    if (data > 0) {
+        return `<div class="positive-number">${data.toFixed(precision)}${postfix}</div>`
+    }
+    else if (data === 0) {
+        return `<div>${data.toFixed(precision)}${postfix}</div>`
+    }
+    else {
+        return `<div class="negative-number">${data.toFixed(precision)}${postfix}</div>`
+    }
+}
+/**
  * Create recommendation table based on 'DataTables' plugin.
  */
 function initRecommendationTable() {
@@ -11,24 +47,24 @@ function initRecommendationTable() {
             {
                 data: "hero-image",
                 render: function (data) {
-                    return `<img class="image-full-block"  src=${data}></img>`;
+                    return `<img data-value=${data.dotabuffName} class="image-full-block"  src=${defaultHeroImagesPath + data.image_path.split("\\").pop()}></img>`;
                 }
             },
             {
                 data: "hero-result",
-                render: DataTable.render.number(null, null, 2, null, "%")
+                render: customRenderNumber50Average
             },
             {
                 data: "hero-general-winrate",
-                render: DataTable.render.number(null, null, 2, null, "%")
+                render: customRenderNumberZeroAverage
             },
             {
                 data: "hero-counter-winrate",
-                render: DataTable.render.number(null, null, 2, null, "%")
+                render: customRenderNumberZeroAverage
             },
             {
-                data: "hero-additional-winrate",
-                render: DataTable.render.number(null, null, 2, null, "%")
+                data: "hero-extra-points",
+                render: customRenderNumberZeroAverage
             },
         ]
     });
@@ -57,11 +93,11 @@ function updateRecommendationTable() {
     for (const hero of mainObjects.recommendationList.heroList) {
         if (hero.visibility) {
             newRow = {
-                "hero-image": defaultHeroImagesPath + hero.image_path.split("\\").pop(),
+                "hero-image": hero ,
                 "hero-result": hero.getFullWinrateSum(),
                 "hero-general-winrate": hero.generalWinrate,
                 "hero-counter-winrate": hero.counterWinrate,
-                "hero-additional-winrate": hero.additionalWinrate
+                "hero-extra-points": hero.teamCorrectionWinrate + hero.additionalWinrate
             };
             dataArray.push(newRow);
         }
@@ -87,6 +123,6 @@ function makeRecalculationAndUpdateTable() {
         mainObjects.rolesAnySet,
         mainObjects.rolesNecessarySet
     )
-    
+
     updateRecommendationTable();
 }
